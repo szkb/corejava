86dca16db6438879b390b7417078a604025d7b44
wuhang <hangwu@ctrip.com>
2019-11-24 14:42:54 +0800

    create corejava project

---
+++ concurrence/src/test/java/com/study/concurrence/ConcurrenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.concurrence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
